44728eaa7b67d67e184340fa7452326b06ce120a
vinimedrades <est.viniciusmedrades20@gmail.com>
2025-08-21 13:43:44 -0300

    Update app.py
    Código finalizado para amostra

--- app.py
+++ app.py
@@ -4,55 +4,57 @@ from urllib.parse import quote
 import webbrowser
 from time import sleep
 import pyautogui
-import os
-import datetime
 
-# Abrir o WhatsApp Web
+# Abrir o WhatsApp Web no navegador
 webbrowser.open('https://web.whatsapp.com/')
 sleep(30)
 
-# Carregar planilha
+# Carregando a planilha Excel
 workbook = openpyxl.load_workbook('clientes.xlsx')
 pagina_clientes = workbook['clientes']
 
-# Loop para envio de mensagens
+# Loop principal para enviar mensagens um por um
 for linha in pagina_clientes.iter_rows(min_row=2):
     nome = linha[0].value
     telefone = linha[1].value
     vencimento = linha[2].value
     status = linha[3].value
 
-    # Pula se já está com status 'ok'
+
+    # Pula se já está com status 'ok'(para nao mandar repetido)
     if status == 'ok':
         continue
 
-    # Verifica se há data de vencimento
+    # Verifica se há data de vencimento e  Cria a mensagem personalizada
     if vencimento:
         mensagem = (
-            f'Olá {nome}, seu boleto vence no dia {vencimento.strftime("%d/%m/%Y")}. '
-            'Favor pagar no link https://www.link_do_pagamento.com'
+            f'Olá {nome} tudo bem ?, seu boleto vence no dia {vencimento.strftime("%d/%m/%Y")}. '
+            'Chave pix: 4199999-9999'
         )
     else:
         print(f'⚠️ Cliente {nome} está sem data de vencimento. Pulando...')
         continue
 
-    # Envia mensagem via WhatsApp
+      # Tenta enviar a mensagem via WhatsApp e cria link do whatsapp
     try:
         link_mensagem_whatsapp = f'https://web.whatsapp.com/send?phone={telefone}&text={quote(mensagem)}'
-        webbrowser.open(link_mensagem_whatsapp)
-        sleep(5)
-        pyautogui.press('enter')
-        sleep(5)
-        pyautogui.hotkey('ctrl', 'w')
-        sleep(5)
+        webbrowser.open(link_mensagem_whatsapp)                 # Abre o link no navegador
+        sleep(5)                                              
+        pyautogui.press('enter')                                # Pressiona Enter para enviar a mensagem
+        sleep(5)                                               
+        pyautogui.hotkey('ctrl', 'w')                           # Fecha a aba do WhatsApp
+        sleep(5)                                                
 
-        # Atualiza o status para apenas 'ok' — sem números
+        # Atualiza o status na planilha para apenas 'ok'
         linha[3].value = "ok"
-
+        
+    #Se tuver algum erro de envio ira para a planilha de erros
     except Exception as e:
         print(f'Não foi possível enviar mensagem para {nome}: {e}')
         with open('erros.csv','a', newline='', encoding='utf-8') as arquivo:
             arquivo.write(f'{nome},{telefone}\n')
+            
+
 
 # Salva planilha
 workbook.save('clientes.xlsx')
